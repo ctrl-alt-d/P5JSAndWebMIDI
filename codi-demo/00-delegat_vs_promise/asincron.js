39f27faf0922bf9753a86237c1900c2ee65251d5
dani herrera <ctrl.alt.d@gmail.com>
2019-01-27 09:45:49 +0100

    adding async to demo

--- codi-demo/00-delegat_vs_promise/asincron.js
+++ codi-demo/00-delegat_vs_promise/asincron.js
@@ -1,19 +1,25 @@
 
-function escalfaForn_as_promise( temperatura  ) {
-    console.log("començo a escalfar (v promise)");
+function escalfaForn_v3( temperatura  ) {
+    console.log("començo a escalfar (v3)");
     let p = new Promise(  
         function ( delegat ) { 
-             setTimeout( delegat, temperatura * 10 ); 
+             setTimeout( delegat, temperatura * 10 );
         },
     );
     return p;
 }
 
-function avisa_v2() {
-    console.log("el forn està calent (v2)");
+function avisa_v3() {
+    console.log("el forn està calent (v3)");
 }
 
-escalfaForn_as_promise(200).then(avisa_v2);
+async function escalfa_i_avisa() {
+    await escalfaForn_v3(200);
+    avisa_v3();
+}
+
+escalfa_i_avisa();
+
 
 /* -- Altres exemples -- */
 
